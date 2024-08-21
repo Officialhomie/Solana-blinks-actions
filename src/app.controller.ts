8ee7e3968563dc1311904df33993a833526ede71
OneTrueHomie <vicigwilo@gmail.com>
2024-08-21 01:33:25 +0100

    finished solana blinks and actions

---
+++ src/app.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Post, Query, Req } from '@nestjs/common';
+import { AppService } from './app.service';
+
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  async getReq(
+    @Req() req: Request,
+  ){
+    return await this.appService.getReq(req);
+  }
+
+  @Post()
+  async postReq(
+    @Req() req: Request,
+    @Query("amount") amount: number 
+  ) {
+    return await this.appService.postReq(amount, req)
+  }
+}
